498d080e18eb820456d5d296ac6bfb09509abe2c
ajmengistu <a_mengistu@outlook.com>
2019-10-25 17:20:30 -0400

    Commented out DisabledTracing to disable debug info in browser & added comments to app-routing.module.ts

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { errorRoute, navbarRoute } from './layouts';
-import { DEBUG_INFO_ENABLED } from 'app/app.constants';
+// import { DEBUG_INFO_ENABLED } from 'app/app.constants';
 
+// Load all the routes from navbarRoute:Route and errorRoute:Route[] into
+// LAYOUT_ROUTES:Route[].
 const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
+
+// Valid routes with /admin/[route-name] that are children of
+// admin route will provide their own route rules
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: './admin/admin.module#MoviemanagerAdminModule'
+  },
+  ...LAYOUT_ROUTES
+];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(
-      [
-        {
-          path: 'admin',
-          loadChildren: './admin/admin.module#MoviemanagerAdminModule'
-        },
-        ...LAYOUT_ROUTES
-      ],
-      { enableTracing: DEBUG_INFO_ENABLED }
+      routes
+      // Uncomment to see Debuging info in Browser console.
+      // { enableTracing: DEBUG_INFO_ENABLED }
     )
   ],
   exports: [RouterModule]
