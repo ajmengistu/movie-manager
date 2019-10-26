5a20c22750d5dd51eb23c9c6bc6910894e7e42e7
ajmengistu <a_mengistu@outlook.com>
2019-10-25 20:13:17 -0400

    Commented out the pacman loading feature to replace it with angular-material

--- src/main/webapp/app/app.main.ts
+++ src/main/webapp/app/app.main.ts
@@ -8,6 +8,15 @@ if (module['hot']) {
   module['hot'].accept();
 }
 
+// PURPOSE of this file: app.main.ts: similar to MAIN() in JAVA or C
+// This purpose of this class is to bootstrap this app. It loads
+// everything and the start up of the app.
+
+// Sets MoviemanagerAppModule is the root module for boostrapping
+// MoviemanagerAppModule -> bootstrap:[JhiComponent] -> finds selector
+// of the bootstrapped component JhiComponent in DOM and initializes
+// the component.
+// Bootstrapping:
 platformBrowserDynamic()
   .bootstrapModule(MoviemanagerAppModule, { preserveWhitespaces: true })
   .then(success => console.log(`Application started`))
